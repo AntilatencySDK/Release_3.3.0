466b6755822a801bd5f2557350860ac27eebba00
Antilatency CI <ci@antilatency.com>
2021-10-01 14:12:57 +0300

    Upload new SDK subset

---
+++ Runtime/Api/Antilatency.Alt.Environment.HorizontalGrid.cs
@@ -0,0 +1,65 @@
+//Copyright 2021, ALT LLC. All Rights Reserved.
+//This file is part of Antilatency SDK.
+//It is subject to the license terms in the LICENSE file found in the top-level directory
+//of this distribution and at http://www.antilatency.com/eula
+//You may not use this file except in compliance with the License.
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+#pragma warning disable IDE1006 // Do not warn about naming style violations
+#pragma warning disable IDE0017 // Do not suggest to simplify object initialization
+using System.Runtime.InteropServices; //GuidAttribute
+namespace Antilatency.Alt.Environment.HorizontalGrid {
+
+[Guid("14a7b13f-b51c-46b3-abf7-623db3a9fb86")]
+public interface ILibrary : Antilatency.Alt.Environment.IEnvironmentConstructor {
+}
+public static class Library{
+    [DllImport("AntilatencyAltEnvironmentHorizontalGrid")]
+    private static extern Antilatency.InterfaceContract.ExceptionCode getLibraryInterface(System.IntPtr unloader, out System.IntPtr result);
+    public static ILibrary load(){
+        System.IntPtr libraryAsIInterfaceIntermediate;
+        getLibraryInterface(System.IntPtr.Zero, out libraryAsIInterfaceIntermediate);
+        Antilatency.InterfaceContract.IInterface libraryAsIInterface = new Antilatency.InterfaceContract.Details.IInterfaceWrapper(libraryAsIInterfaceIntermediate);
+        var library = libraryAsIInterface.QueryInterface<ILibrary>();
+        libraryAsIInterface.Dispose();
+        return library;
+    }
+}
+namespace Details {
+	public class ILibraryWrapper : Antilatency.Alt.Environment.Details.IEnvironmentConstructorWrapper, ILibrary {
+		private ILibraryRemap.VMT _VMT = new ILibraryRemap.VMT();
+		protected new int GetTotalNativeMethodsCount() {
+		    return base.GetTotalNativeMethodsCount() + typeof(ILibraryRemap.VMT).GetFields().Length;
+		}
+		public ILibraryWrapper(System.IntPtr obj) : base(obj) {
+		    _VMT = LoadVMT<ILibraryRemap.VMT>(base.GetTotalNativeMethodsCount());
+		}
+	}
+	public class ILibraryRemap : Antilatency.Alt.Environment.Details.IEnvironmentConstructorRemap {
+		public new struct VMT {
+			#pragma warning disable 0649
+			#pragma warning restore 0649
+		}
+		public new static readonly NativeInterfaceVmt NativeVmt;
+		static ILibraryRemap() {
+			var vmtBlocks = new System.Collections.Generic.List<object>();
+			AppendVmt(vmtBlocks);
+			NativeVmt = new NativeInterfaceVmt(vmtBlocks);
+		}
+		protected static new void AppendVmt(System.Collections.Generic.List<object> buffer) {
+			Antilatency.Alt.Environment.Details.IEnvironmentConstructorRemap.AppendVmt(buffer);
+			var vmt = new VMT();
+			buffer.Add(vmt);
+		}
+		public ILibraryRemap() { }
+		public ILibraryRemap(System.IntPtr context, ushort lifetimeId) {
+			AllocateNativeInterface(NativeVmt.Handle, context, lifetimeId);
+		}
+	}
+}
+
+
+}
